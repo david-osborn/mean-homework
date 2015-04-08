f45db89469c22b4ed38c458e34dfa8eacda6ec41
David <davidosborn89@gmail.com>
2015-04-07 18:22:48 -0600

    Pretest

---
+++ pretest.js
@@ -0,0 +1,7 @@
+var firstName="David"
+var lastName="Osborn"
+console.log(lastName + ', ' + firstName)
+
+
+var person = {};
+ // I'm not finished, though we were practicing pushing things so I did :)
