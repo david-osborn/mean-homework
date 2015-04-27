7540c71c407c8e8ec46fe8751a4e355787b0f0f1
davidosborn89 <davidosborn89@gmail.com>
2015-04-27 17:46:58 -0600

    Delete pretest.js

--- pretest.js
+++
@@ -1,7 +0,0 @@
-var firstName="David"
-var lastName="Osborn"
-console.log(lastName + ', ' + firstName)
-
-
-var person = {};
- // I'm not finished, though we were practicing pushing things so I did :)
